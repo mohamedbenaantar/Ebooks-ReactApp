60561921df9ce023b1f292d61acd5503a09c17c7
mohamedbenaantar <mohamedbenantar@gmail.com>
2023-09-25 12:01:23 +0100

    Design the Home Page

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import { Hero } from "./components/Hero"
+import { FeaturedProducts } from './components/FeaturedProducts'
+import { Testimonials } from "./components/Testimonials"
+import { Faq } from "./components/Faq"
 export const HomePage = () => {
   return (
     <main>
-      <div>Home</div>
+      <Hero/>
+      <FeaturedProducts/>
+      <Testimonials/>
+      <Faq/>
     </main>
   )
 }
